3f03c87ba4e9930ceed9e9cf8a8a03921cfa0399
«Mikhail» <kopfgolova@gmail.com>
2022-05-25 12:03:20 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -5,10 +5,34 @@
 */
 int [] FillArray(int length)
 {
-    
+    int [] array = new int [length];
+    Random rnd = new Random();
+    for(int i = 0; i < length; i++)
+        array[i] = rnd.Next(100, 1000);
+    return array;
+}
+void PrintArray(int [] arr)
+{
+    foreach(int item in arr)
+        Console.Write($"{item} ");
+}
+int CountOfEven(int [] col)
+{
+    int count = 0;
+    for(int i = 0; i < col.Length; i++)
+        if(col[i] % 2 == 0) count++;
+    return count;
 }
 
 
 
+Console.WriteLine("Ведите размерность массива: ");
+int len = int.Parse(Console.ReadLine());
+int [] array = FillArray(len);
+int count = CountOfEven(array);
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Полученный массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine($"Количество четных чисел в массиве равно {count}");
